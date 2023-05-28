36779fc5a523882e210ccdd24ef25817576bd684
guilhermeavila77 <guialmeidaavila@gmail.com>
2023-05-28 18:35:15 -0300

    Feature de desligar

--- Conexão ESP8266/scripts/botoes.js
+++
@@ -1,6 +0,0 @@
-const ligar = document.getElementById('ligar');
-
-ligar.addEventListener('click', () => {
-    console.log('botão clicado')
-    window.location.href = '/ligar'; // Redireciona para '/nova-rota'
-});
